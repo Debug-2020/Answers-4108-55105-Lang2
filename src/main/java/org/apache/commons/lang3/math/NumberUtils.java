34e40ec55793603e372e7fb1d2c8feac6dcc526a
Debug-2020 <debug2020@protonmail.com>
2020-01-02 15:11:47 +0800

    4108-55105-Lang2-result1575164383276

--- src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -465,10 +465,10 @@ public class NumberUtils {
         }
         if (pfxLen > 0) { // we have a hex number
             final int hexDigits = str.length() - pfxLen;
-            if (hexDigits >= 16) { // too many for Long
+            if (hexDigits > 16) { // too many for Long
                 return createBigInteger(str);
             }
-            if (hexDigits >= 8) { // too many for an int
+            if (hexDigits > 8) { // too many for an int
                 return createLong(str);
             }
             return createInteger(str);
