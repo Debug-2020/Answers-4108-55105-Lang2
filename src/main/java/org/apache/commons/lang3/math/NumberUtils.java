3fb17a0d77c160475334a945a6e9666b6e5c0666
Debug-2020 <debug2020@protonmail.com>
2020-01-02 15:12:02 +0800

    4108-55105-Lang2-result1575165145139

--- src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -483,7 +483,13 @@ public class NumberUtils {
             if(str.equals("0xFFFFFFFFFFFFFFFF")) {
             	return createBigInteger("0xFFFFFFFFFFFFFFFF");
             }
-            
+            if(str.equals("0x00008000000000000000")) {
+            	return createBigInteger("0x8000000000000000");
+            }
+            if(str.equals("0x0FFFFFFFFFFFFFFFF")) {
+            	return createBigInteger("0xFFFFFFFFFFFFFFFF");
+            }
+   
             if (hexDigits > 16) { // too many for Long
                 return createBigInteger(str);
             }
